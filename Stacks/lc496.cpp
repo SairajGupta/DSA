78fe45571548fd9804f06c343713b6ebad394647
Sairaj Gupta <srgupta1235@gmail.com>
2026-02-11 10:47:06 +0530

    lc 496

--- Stacks/lc496.cpp
+++ Stacks/lc496.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> nextGreater;
+        int n = nums2.size();
+        unordered_map<int,int> mp;
         stack<int> st;
         
-        for (int i = nums2.size() - 1; i >= 0; --i) {
-            while (!st.empty() && st.top() <= nums2[i]) {
+        for(int i = n-1;i>=0; i--){
+            while(!st.empty() && st.top() <= nums2[i]){
                 st.pop();
             }
-            nextGreater[nums2[i]] = st.empty() ? -1 : st.top();
+            if(st.empty()){
+                mp[nums2[i]] = -1;
+            }
+            else{
+                mp[nums2[i]]= st.top();
+            }
             st.push(nums2[i]);
         }
-        
         vector<int> result;
-        for (int num : nums1) {
-            result.push_back(nextGreater[num]);
+        for(int i = 0; i < nums1.size();i++){
+            result.push_back(mp[nums1[i]]);
         }
-        
         return result;
     }
 };
