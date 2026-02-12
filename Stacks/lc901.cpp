99f5f231bb8e0090cf66bf4dff5b85dfffd235fa
Sairaj Gupta <srgupta1235@gmail.com>
2026-02-12 13:54:29 +0530

    lc 901

---
+++ Stacks/lc901.cpp
@@ -0,0 +1,18 @@
+class StockSpanner {
+public:
+    stack<pair<int,int>> st;
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        int currSpan = 1;
+
+        while(!st.empty() && st.top().first <= price){
+            currSpan += st.top().second;
+            st.pop();
+        }
+        st.push({price,currSpan});
+        return currSpan;
+    }
+};
