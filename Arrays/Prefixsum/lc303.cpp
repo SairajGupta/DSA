6873f62f428022d33f19ccf82b1b4bbb0fc114b3
Sairaj Gupta <srgupta1235@gmail.com>
2026-06-22 12:21:23 +0530

    LC 303

---
+++ Arrays/Prefixsum/lc303.cpp
@@ -0,0 +1,22 @@
+class NumArray {
+public:
+    vector<int> num;
+    NumArray(vector<int>& nums) {
+        int n = nums.size();
+        num=nums;
+        for(int i = 1;i < n;i++){
+            num[i]=num[i-1]+num[i];
+        }
+    }
+    
+    int sumRange(int left, int right) {
+        if (left==0) return num[right];
+        return num[right]-num[left-1];
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
